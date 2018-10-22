beedd3f8453f769085ab4b7136c268bbb28e8f40
CloudRex <cloudrex@outlook.com>
2018-10-22 15:54:40 -0400

    Initial commit

---
+++ src/node-tree.ts
@@ -0,0 +1,92 @@
+export enum NodeType {
+    Root = "PRN",
+    MethodCall = "MEC",
+    StringLiteral = "STL",
+    NumberLiteral = "NBL"
+}
+
+export type NodeValue = string | number | any;
+
+export type TreeNode = {
+    readonly type: NodeType;
+
+    value: NodeValue;
+}
+
+export default class NodeTree {
+    private readonly tree: TreeNode;
+    private readonly path: TreeNode[];
+
+    private currentNode: TreeNode;
+
+    public constructor() {
+        this.tree = NodeTree.createEmptyNode(NodeType.Root);
+        this.currentNode = this.tree;
+        this.path = [this.currentNode];
+    }
+
+    public onRoot(): boolean {
+        return this.currentNode.type === NodeType.Root;
+    }
+
+    public getParent(): TreeNode {
+        if (this.onRoot() || this.path.length - 2 < 0) {
+            throw new RangeError("Canot get parent of root node");
+        }
+
+        return this.path[this.path.length - 2];
+    }
+
+    public hasChild(name: string): boolean {
+        return typeof this.currentNode.value === "object" && Object.keys(this.currentNode.value).includes(name);
+    }
+
+    public getChild(name: string): TreeNode {
+        if (!this.hasChild(name)) {
+            throw new Error(`Current node does not have '${name}' as a child`);
+        }
+
+        return this.currentNode.value[name];
+    }
+
+    public parent(): this {
+        this.currentNode = this.getParent();
+        this.removeLastPath();
+
+        return this;
+    }
+
+    public child(name: string): this {
+        this.currentNode = this.getChild(name);
+        this.pushNewPath();
+
+        return this;
+    }
+
+    public setChild(name: string, node: TreeNode): this {
+        if (this.hasChild(name)) {
+            throw new Error(`Child with name '${name}' already exists`);
+        }
+
+        return this;
+    }
+
+    private removeLastPath(): this {
+        this.path.pop();
+
+        return this;
+    }
+
+    private pushNewPath(): this {
+        this.path.push(this.currentNode);
+
+        return this;
+    }
+
+    public static createEmptyNode(type: NodeType): TreeNode {
+        return {
+            type,
+            value: {}
+        };
+    }
+}
